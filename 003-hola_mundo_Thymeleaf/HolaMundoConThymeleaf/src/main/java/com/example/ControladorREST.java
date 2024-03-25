10b6e66373c38d2d674643f09c3354aaadc4bbac
kolodani <danielvinzia@gmail.com>
2024-03-25 11:25:32 -0300

    agregando cambios desde application properties

--- 003-hola_mundo_Thymeleaf/HolaMundoConThymeleaf/src/main/java/com/example/ControladorREST.java
+++ 003-hola_mundo_Thymeleaf/HolaMundoConThymeleaf/src/main/java/com/example/ControladorREST.java
@@ -1,18 +1,25 @@
 package com.example;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class ControladorREST {
 
+    @Value("${indice.hola2}")
+    private String hola2;
+
     @GetMapping("/")
-    public String comienzo() {
-        // el mensaje del log se mostrará en la consola
+    public String comienzo(Model model) {
+        String hola = "Estamos probando mensajes";
         log.info("Estoy ejecutando el controlador MVC");
-        // el mensaje del return se mostrará en el navegador
+        model.addAttribute("hola", hola);
+        model.addAttribute("hola2", hola2);
         return "indice";
     }
 }
