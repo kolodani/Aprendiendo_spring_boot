2a669bdb067af3dee4f64b138fe4567bde7e1053
kolodani <danielvinzia@gmail.com>
2024-03-25 10:30:58 -0300

    ultimas modificaciones de la primera parte

--- 002-primer_proyecto/HelloSpring/src/main/java/com/example/ControladorREST.java
+++ 002-primer_proyecto/HelloSpring/src/main/java/com/example/ControladorREST.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@Slf4j
 public class ControladorREST {
 
     @GetMapping("/")
     public String comienzo() {
-        return "¡Hola Mundo en Spring!";
+        // el mensaje del log se mostrará en la consola
+        log.info("Estoy ejecutando el controlador REST");
+        log.debug("MAS INFORMACION");
+        // el mensaje del return se mostrará en el navegador
+        return "¡Mi Primer Hola Mundo en Spring!";
     }
 }
